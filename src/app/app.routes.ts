f433448cf540087f5a31347b947c87ed77703bb2
alencar-bianca <alencar-bianca@hotmail.com>
2024-05-10 08:28:44 -0300

    mudanca de pasta

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { IndexComponent } from './pages/index/index.component';
-import { CharacterComponent } from './pages/character/character.component';
+import { CharacterComponent } from './pages/characters/characters-all/character.component';
+
+
 
 export const routes: Routes = [
   {
